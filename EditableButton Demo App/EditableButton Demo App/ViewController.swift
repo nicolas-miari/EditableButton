f5b10c53daf4cd99cc5627b167316e3da9b80f57
Nicolás Miari <nicolasmiari@gmail.com>
2017-02-01 11:38:33 +0900

    Added text button

--- EditableButton Demo App/EditableButton Demo App/ViewController.swift
+++ EditableButton Demo App/EditableButton Demo App/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /// Edited with keyboard
+    @IBOutlet weak var textButton: EditableButton!
+
+    /// Edited with date picker
     @IBOutlet weak var dateButton: EditableButton!
 
     // MARK: - UIViewController
@@ -18,13 +22,38 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
+        configureTextButton()
+        configureDateButton()
+    }
+
+    private func configureTextButton() {
+
+        textButton.setTitle("Sample Text", for: .normal)
+
+        let textToolbar = UIToolbar(frame: CGRect.zero)
+        textToolbar.barStyle = .default
+
+        let textDoneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(ViewController.textDone(_:)))
+        let textSpacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+
+        textToolbar.items = [textSpacer, textDoneButton]
+        textToolbar.sizeToFit()
+
+        textButton.inputAccessoryView = textToolbar
+    }
+
+    private func configureDateButton() {
+
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         dateButton.setTitle(dateFormatter.string(from: Date()), for: .normal)
 
         let datePicker = UIDatePicker()
         datePicker.datePickerMode = .date
-        //dateButton.inputView = datePicker
+        dateButton.inputView = datePicker
         datePicker.addTarget(self, action: #selector(ViewController.dateChanged(_:)), for: .valueChanged)
 
         let dateToolbar = UIToolbar(frame: CGRect.zero)
@@ -52,5 +81,9 @@ class ViewController: UIViewController {
 
     @IBAction func dateDone(_ sender: UIBarButtonItem) {
         dateButton.resignFirstResponder()
+    }
+
+    @IBAction func textDone(_ sender: UIBarButtonItem) {
+        textButton.resignFirstResponder()
     }
 }
